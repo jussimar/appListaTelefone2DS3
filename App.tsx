7fa68e66178b696202ddbccd03a34cc5f93aed65
jussimar <jussimar@jussimarleal.com.br>
2025-05-30 22:00:09 -0300

    criação de lista de contatos e componte de contato

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Contatos from './componentes/Contatos';
+const contatos = [
+  {id:"1",nome:"Augusto",tel:"(13) 99859-9874"},
+  {id:"2",nome:"Maria",tel:"(13) 75859-9874"},
+  {id:"3",nome:"José",tel:"(13) 87859-9874"},
+  {id:"4",nome:"Ana",tel:"(13) 89859-9874"},
+  {id:"5",nome:"Paulo",tel:"(13) 95859-9874"},
+  {id:"6",nome:"Orelha",tel:"(13) 95559-9874"},
+  {id:"7",nome:"Jussimar",tel:"(13) 96859-9874"},
+  {id:"8",nome:"Jonas",tel:"(13) 85859-9874"},
+]
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Text style={styles.titulo}>Lista de Contatos</Text>
+      
+      <View style={styles.lista}>
+        <FlatList 
+          data={contatos}
+          keyExtractor={item=>item.id}
+          renderItem={({item}) => <Contatos tel={item.tel} nome={item.nome}/>}
+        />
+      </View>
+      
+      
+      
     </View>
   );
 }
@@ -17,4 +37,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  lista:{
+    height:500,
+    width:'90%',
+    marginLeft:'5%'
+  },
+  titulo:{
+    fontSize:30,
+    fontWeight:'bold'
+  }
+  
 });
